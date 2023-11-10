bcba305810eeced3a1cc73c9bba9b87cdf74de8e
0leg-zharkov <privedenie1748@mail.ru>
2023-11-10 20:40:48 +0300

    добавил надпись ввода

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
+        System.out.println("Enter a number");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
